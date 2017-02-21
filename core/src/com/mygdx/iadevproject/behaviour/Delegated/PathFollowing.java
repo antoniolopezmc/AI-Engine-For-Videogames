29421df7fe97916798266651a859b07eaddd5556
José María Sánchez Salas <josemaria.sanchezsalas@gmail.com>
2017-02-21 12:40:15 +0100

    Comportamientos delegados creados

---
+++ core/src/com/mygdx/iadevproject/behaviour/Delegated/PathFollowing.java
@@ -0,0 +1,15 @@
+package com.mygdx.iadevproject.behaviour.Delegated;
+
+import com.mygdx.iadevproject.behaviour.Behaviour;
+import com.mygdx.iadevproject.modelo.Character;
+import com.mygdx.iadevproject.steering.Steering;
+
+public class PathFollowing implements Behaviour {
+
+	@Override
+	public Steering getSteering(Character source, Character target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
