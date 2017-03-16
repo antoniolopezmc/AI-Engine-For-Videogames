c02e9d13f8a722530212c15b6c35741eb22c7674
José María Sánchez Salas <josemaria.sanchezsalas@gmail.com>
2017-03-16 17:23:13 +0100

    Carpetas de test creadas. Tests para los movimientos uniformes hechos y funcionando!

---
+++ desktop/test/com/mygdx/iadevproject/behaviour/NoAcceleratedUnifMov/TestArrive_NoAcceleratedLauncher.java
@@ -0,0 +1,13 @@
+package com.mygdx.iadevproject.behaviour.NoAcceleratedUnifMov;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class TestArrive_NoAcceleratedLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		//config.width = 2000;
+		//config.height = 1000;
+		new LwjglApplication(new TestArrive_NoAccelerated(), config);
+	}
+}
