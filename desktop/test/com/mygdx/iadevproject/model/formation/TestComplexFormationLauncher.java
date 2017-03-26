625c93d55d7f2668d1cdca1847f907244e202139
Antonio López Martínez-Carrasco <antonio_l_m_c_1995@hotmail.es>
2017-03-26 19:09:32 +0200

    Modificación de la clase Formation para que siga funcionando el patrón composite. PROBADO. FUNCIONA.

---
+++ desktop/test/com/mygdx/iadevproject/model/formation/TestComplexFormationLauncher.java
@@ -0,0 +1,14 @@
+package com.mygdx.iadevproject.model.formation;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.iadevproject.model.formation.TestCircularFormation;
+
+public class TestComplexFormationLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		//config.width = 2000;
+		//config.height = 1000;
+		new LwjglApplication(new TestComplexFormation(), config);
+	}
+}
