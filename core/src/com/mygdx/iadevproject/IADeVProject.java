09e695f1f27f2c7f57b0cda01be72c2a387f5c55
José María Sánchez Salas <josemaria.sanchezsalas@gmail.com>
2017-04-17 16:13:17 +0200

    Añadido el mapa. Ahora solamente se muestra el mapa, no está controlada la cámara, por eso se sale del mapa

--- core/src/com/mygdx/iadevproject/IADeVProject.java
+++ core/src/com/mygdx/iadevproject/IADeVProject.java
@@ -1,6 +1,7 @@
 package com.mygdx.iadevproject;
 
 import java.util.Set;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
@@ -14,6 +15,10 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector3;
 import com.mygdx.iadevproject.aiReactive.arbitrator.PriorityArbitrator;
 import com.mygdx.iadevproject.model.Character;
@@ -22,9 +27,9 @@ import com.mygdx.iadevproject.model.WorldObject;
 public class IADeVProject extends ApplicationAdapter {
 	
 	// Anchura del mapa.
-	public static int WIDTH = 1000;
+	public static int WIDTH = 2048;
 	// Altura del mapa
-	public static int HEIGHT = 1000;
+	public static int HEIGHT = 2048;
 	public static int[][] MAP_OF_COSTS = new int[WIDTH][HEIGHT];
 	public static int INFINITY = Integer.MAX_VALUE;
 	public static int DEFAULT_COST = 1;
@@ -32,11 +37,16 @@ public class IADeVProject extends ApplicationAdapter {
 	public static List<WorldObject> worldObjects;		// Objetos del mundo
 	public static Set<WorldObject> selectedObjects; 	// Lista de objetos seleccionados
 	public static OrthographicCamera camera;			// Cárama (es pública para que se pueda acceder el InputProcessorIADeVProject)
-	public static InputProcessorIADeVProject inputProcessor;		// InputProcessor
+	
 	
 	private SpriteBatch batch;
 	private BitmapFont font;
 	private ShapeRenderer renderer;
+	private TiledMap tiledMap;
+    private TiledMapRenderer tiledMapRenderer;
+	
+	
+	private InputProcessorIADeVProject inputProcessor;		// InputProcessor
 	
 	public static Character drop, bucket;
 	
@@ -48,21 +58,23 @@ public class IADeVProject extends ApplicationAdapter {
 		Gdx.input.setInputProcessor(inputProcessor);
 	
 		selectedObjects = new HashSet<WorldObject>();
-		worldObjects = new LinkedList<WorldObject>();
+		worldObjects 	= new LinkedList<WorldObject>();
 		
 		float w = Gdx.graphics.getWidth();
         float h = Gdx.graphics.getHeight();
 
         // Constructs a new OrthographicCamera, using the given viewport width and height
         // Height is multiplied by aspect ratio.
-        camera = new OrthographicCamera(w, h);
-        batch = new SpriteBatch();
-        font = new BitmapFont();
-        renderer = new ShapeRenderer();
-        
+        camera 				= new OrthographicCamera(w, h);
         camera.position.set(camera.viewportWidth / 2f, camera.viewportHeight / 2f, 0);
         camera.update();
-
+        
+        batch 				= new SpriteBatch();
+        font 				= new BitmapFont();
+        renderer 			= new ShapeRenderer();
+        tiledMap 			= new TmxMapLoader().load("../core/assets/map.tmx");
+        tiledMapRenderer 	= new OrthogonalTiledMapRenderer(tiledMap);
+        
         drop = new Character(new PriorityArbitrator(1e-5f), new Texture(Gdx.files.internal("../core/assets/droplet.png")));
     	drop.setBounds(100.0f, 100.0f, 64.0f, 64.0f);
         drop.setOrientation(60.0f);
@@ -73,8 +85,7 @@ public class IADeVProject extends ApplicationAdapter {
         bucket.setOrientation(60.0f);
         bucket.setVelocity(new Vector3(0,0.0f,0));
 
-        worldObjects.add(drop);
-        worldObjects.add(bucket);
+        addToWorldObjectList(drop, bucket);
 	}
 	
 	@Override
@@ -84,22 +95,42 @@ public class IADeVProject extends ApplicationAdapter {
         // que mientras se pulse una de las teclas de movimiento de la cámara, la cámara se mueva sin tener que estar pulsando
         // cada vez la misma tecla.
         inputProcessor.processKeyPressed();
-		
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        
         camera.update();
         // Estas 2 lineas sirven para que los objetos dibujados actualicen su posición cuando se mueva la cámara. (Que se muevan también).
         batch.setProjectionMatrix(camera.combined);
         renderer.setProjectionMatrix(camera.combined);
+        tiledMapRenderer.setView(camera);
+        tiledMapRenderer.render();
         
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        
         		
         
-        
         batch.begin();
-        drop.draw(batch);
-        bucket.draw(batch);
+        for (WorldObject obj : worldObjects) {
+        	obj.draw(batch);
+        }
         batch.end();
 	}
 
+	@Override
+	public void dispose() {
+		// dispose of all the native resources
+		for (WorldObject obj : worldObjects) {
+			obj.getTexture().dispose();
+		}
+        batch.dispose();
+	}
+	
+	/**
+	 * Método para añadir a la lista de objetos del mundo nuevos objetos.
+	 * @param objects - Nuevos objetos a añadir
+	 */
+	public static void addToWorldObjectList(WorldObject ... objects) {
+		worldObjects.addAll(Arrays.asList(objects));
+	}
+	
 	/**
 	 * Método que limpia la lista de objetos seleccionados.
 	 */
@@ -122,12 +153,4 @@ public class IADeVProject extends ApplicationAdapter {
 		selectedObjects.add(obj);
 	}
 
-	@Override
-	public void dispose() {
-		// dispose of all the native resources
-		for (WorldObject obj : worldObjects) {
-			obj.getTexture().dispose();
-		}
-        batch.dispose();
-	}
 }
