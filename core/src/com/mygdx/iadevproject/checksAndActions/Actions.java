33f5661d09d5ecf8cd77570f033637583a589ab2
José María Sánchez Salas <josemaria.sanchezsalas@gmail.com>
2017-05-09 00:19:17 +0200

    Algunas acciones y comprobaciones realizadas. Falta mañana hablar con Antonio más detalladamente sobre algunas. He añadido dos comprobaciones más: es de mi equipo? y, es del equipo enemigo?

--- core/src/com/mygdx/iadevproject/checksAndActions/Actions.java
+++ core/src/com/mygdx/iadevproject/checksAndActions/Actions.java
@@ -1,7 +1,71 @@
 package com.mygdx.iadevproject.checksAndActions;
 
+import java.util.List;
+
+import com.badlogic.gdx.math.Vector3;
+import com.mygdx.iadevproject.IADeVProject;
+import com.mygdx.iadevproject.aiReactive.behaviour.Behaviour;
+import com.mygdx.iadevproject.aiReactive.behaviour.acceleratedUnifMov.Flee_Accelerated;
+import com.mygdx.iadevproject.aiReactive.behaviour.delegated.PathFollowingWithoutPathOffset;
+import com.mygdx.iadevproject.aiReactive.behaviour.delegated.Wander_Delegated;
+import com.mygdx.iadevproject.aiReactive.pathfinding.PathFinding;
+import com.mygdx.iadevproject.model.Character;
+import com.mygdx.iadevproject.model.WorldObject;
+
 public class Actions {
 	
+	/**
+	 * Método que refleja la acción de huir de un objetivo.
+	 * @param source Personaje que quiere aplicar la acción.
+	 * @param target Objetivo del que se quiere huir
+	 * @param maxAcceleration Máxima aceleración a la que se quiere huir.
+	 * @return El comportamiento correspondiente a la acción de huir.
+	 */
+	public Behaviour flee(Character source, WorldObject target, float maxAcceleration) {
+		return new Flee_Accelerated(source, target, maxAcceleration);
+	}
 	
+	/**
+	 * Método que refleja la acción de ir a curarse a una posición 'position' dada como parámetro
+	 * @param source Personaje que quiere aplicar la acción.
+	 * @param position Posición donde se va a curar.
+	 * @param maxAcceleration Máxima acceleración a la que se quiere ir
+	 * @return El comportamiento correspondiente a la acción de ir a curarse.
+	 */
+	public Behaviour goToCure(Character source, Vector3 position, float maxAcceleration) {
+		PathFinding pf = new PathFinding();
+		List<Vector3> pointsList = pf.applyPathFinding(IADeVProject.MAP_OF_COSTS, IADeVProject.GRID_CELL_SIZE, PathFinding.CHEBYSHEV_DISTANCE, IADeVProject.GRID_WIDTH, IADeVProject.GRID_HEIGHT, 
+				source.getPosition().x, source.getPosition().y, position.x, position.y);
+		
+		return new PathFollowingWithoutPathOffset(source, maxAcceleration, pointsList, 1.0f, PathFollowingWithoutPathOffset.MODO_PARAR_AL_FINAL);
+	}
 	
+	/**
+	 * Método que refleja la acción de apoyar a un aliado
+	 * @param source
+	 * @param target
+	 * @return
+	 */
+	public Behaviour supportAnAlly(Character source, Character target) {
+		return null;
+	}
+	
+	/**
+	 * Método que refleja la acción de patrullar la zona del personaje
+	 * @param source
+	 * @return
+	 */
+	public Behaviour patrolYourArea(Character source) {
+		return null;
+	}
+	
+	/**
+	 * Método que refleja la acción de hacer cosas aleatorias. Básicamente se basa en que el 
+	 * personaje se mueva de manera aleatoria.
+	 * @param source
+	 * @return
+	 */
+	public Behaviour doRandomThings(Character source) {
+		return new Wander_Delegated(source, 10.0f, 10.0f, 10.0f, 30.0f, 1.0f, 100.0f, 30.0f, 30.0f, 45.0f, 10.0f);
+	}
 }
--- core/src/com/mygdx/iadevproject/checksAndActions/Checks.java
+++ core/src/com/mygdx/iadevproject/checksAndActions/Checks.java
@@ -1,7 +1,84 @@
 package com.mygdx.iadevproject.checksAndActions;
 
+import com.mygdx.iadevproject.IADeVProject;
+import com.mygdx.iadevproject.model.Character;
+import com.mygdx.iadevproject.model.Team;
+import com.mygdx.iadevproject.model.WorldObject;
+
 public class Checks {
 	
+	/**
+	 * Método que comprueba si hay enemigos en la base del personaje 'source'
+	 * @param source Personaje que quiere saber si hay enemigos en su base
+	 * @return true si hay personajes en su base, false en caso contrario
+	 */
+	public boolean areThereEnemyInMyBase(Character source) {
+		for (WorldObject obj : IADeVProject.worldObjects) {
+			if (obj instanceof Character) {
+				Character target = (Character)obj;
+				
+				if (isItFromEnemyTeam(source, target)) {
+					
+				}
+			}
+		}
+		return false;
+	}
 	
+	/**
+	 * Método que comprueba si el personaje 'source' se encuentra en su base.
+	 * @param source Personaje que quiere saber si está en su base.
+	 * @return true si está en su base, false en caso contrario.
+	 */
+	public boolean amIInMyBase(Character source) {
+		return false;
+	}
+	
+	/**
+	 * Método que comprueba si el personaje 'source' está en la base del enemigo.
+	 * @param source Personaje que quiere saber si está en la base del enemigo.
+	 * @return true si está en la base enemiga, false en caso contrario.
+	 */
+	public boolean amIInEnemyBase(Character source) {
+		return false;
+	}
+	
+	/**
+	 * Método que comprueba si el personaje 'source' ha recuperado su vida totalmente.
+	 * @param source Personaje que quiere saber si ha recuperado su vida totalmente.
+	 * @return true si hay recuperado toda su vida, false en caso contrario.
+	 */
+	public boolean haveIFullyRecoveredMyHealth(Character source) {
+		return source.getCurrentHealth() == source.getMaxHealth();
+	}
+	
+	/**
+	 * Método que comprueba si el personaje 'source' ha muerto.
+	 * @param source Personaje que quiere saber si ha muerto.
+	 * @return true si ha muerto, false en caso contrario.
+	 */
+	public boolean haveIDead(Character source) {
+		return source.getCurrentHealth() <= 0;
+	}
+	
+	/**
+	 * Método que comprueba si el personaje 'target' es del equipo enemigo al personaje 'source'.
+	 * @param source Personaje que quiere saber si 'target' es del equipo contrario.
+	 * @param target Personaje del que se quiere saber si es del equipo contrario.
+	 * @return true si es del equipo contrario, false en caso contrario.
+	 */
+	public boolean isItFromEnemyTeam(Character source, Character target) {
+		return source.getTeam() == target.getTeam() && target.getTeam() != Team.NEUTRAL;
+	}
+	
+	/**
+	 * Método que comprueba si los dos personajes son del mismo equipo.
+	 * @param source Personaje que realiza la pregunta.
+	 * @param target Personaje del que se quiere saber si es del equipo de 'source'.
+	 * @return true si son del mismo equipo, false en caso contrario.
+	 */
+	public boolean isItFromMyTeam(Character source, Character target) {
+		return source.getTeam() == target.getTeam();
+	}
 
 }
